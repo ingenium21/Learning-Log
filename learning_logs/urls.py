7afb2fad8c03c63db6b5558647d8a446d42389f0
Renato Regalado <renato.regalado@gmail.com>
2020-05-13 07:12:03 -0500

    added single topic and entries

--- learning_logs/urls.py
+++ learning_logs/urls.py
@@ -9,5 +9,7 @@ urlpatterns = [
     # Home Page
     path('', views.index, name='index'),
     #Page that shows all topics
-    path('topics/', views.topics, name='topics')
+    path('topics/', views.topics, name='topics'),
+    #Detail page for a single topic
+    path('topics/<int:topic_id>/', views.topic, name='topic')
 ]
