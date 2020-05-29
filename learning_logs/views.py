2835150349f5a6e332df09eb1036b2ab91ac8eaf
Renato Regalado <renato.regalado@gmail.com>
2020-05-29 06:30:00 -0500

    refactoring code

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -21,8 +21,7 @@ def topic(request, topic_id):
     """Show a single topic and all its entries"""
     topic = Topic.objects.get(id=topic_id)
     # Make sure the topic belongs to the current user
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(request, topic)
 
     entries = topic.entry_set.order_by('-date_added')
     context = {'topic':topic, 'entries': entries}
@@ -51,7 +50,8 @@ def new_topic(request):
 def new_entry(request, topic_id):
     """Add a new entry for a particular topic"""
     topic = Topic.objects.get(id=topic_id)
-
+    check_topic_owner(request, topic)
+    
     if request.method != 'POST':
         # No  data submitted ; create a blank form.
         form = EntryForm()
@@ -73,8 +73,7 @@ def edit_entry(request, entry_id):
     """Edit an existigin entry."""
     entry = Entry.objects.get(id=entry_id)
     topic = entry.topic
-    if topic.owner != request.user:
-        raise Http404
+    check_topic_owner(request, topic)
 
     if request.method != 'POST':
         #Initial request, prefill form with current entry
@@ -88,3 +87,7 @@ def edit_entry(request, entry_id):
     
     context = {'entry': entry, 'topic': topic, 'form': form}
     return render(request, 'learning_logs/edit_entry.html', context)
+
+def check_topic_owner(request, topic):
+    if topic.owner != request.user:
+        raise Http404
